e693402c190ec310231827bf83f39a0a11707bb8
Arithmetics <brock.m.tillotson@gmail.com>
2020-05-25 18:21:30 -0700

    regorded files for clarity

--- get_mfl_draft_data.rb
+++ get_mfl_draft_data.rb
@@ -1,14 +1,14 @@
-require 'nokogiri'
-require 'open-uri'
-require 'csv'
+require "nokogiri"
+require "open-uri"
+require "csv"
 
-
+# takws a row of mfl draft and converts it to a map/object of a 'draft pick'
 def convert_row_to_draft_pick(row, league_id, year)
   draft_pick = {}
   cells = row.css("td")
   draft_pick[:pick_number] = cells[1].text
-  draft_pick[:owner_id] = cells[2].css("a")[0]['class'].gsub("franchise_", "").to_i
-  draft_pick[:mfl_player_id] = cells[3].css("a")[0]['href'].gsub("javascript:launch_player_modal('#{league_id}','", "").gsub("');", "").to_i
+  draft_pick[:owner_id] = cells[2].css("a")[0]["class"].gsub("franchise_", "").to_i
+  draft_pick[:mfl_player_id] = cells[3].css("a")[0]["href"].gsub("javascript:launch_player_modal('#{league_id}','", "").gsub("');", "").to_i
 
   player_info = cells[3].css("a").text
 
@@ -21,7 +21,6 @@ def convert_row_to_draft_pick(row, league_id, year)
   return draft_pick
 end
 
-
 ##### MAIN #####
 
 league_id = 20896
@@ -30,12 +29,11 @@ doc = Nokogiri::HTML(open("http://www67.myfantasyleague.com/#{draft_year}/option
 rows = doc.css(".report")[0].css("tr")
 draft_picks = []
 
-rows.each_with_index do |row, index| 
-  if index != 0 
+rows.each_with_index do |row, index|
+  if index != 0
     draft_picks.push(convert_row_to_draft_pick(row, league_id, draft_year))
   end
 end
-
 
 CSV.open("player_crosswalk_#{draft_year}.csv", "w") do |csv|
   csv << ["player_name", "mfl_player_id", "fft_player_id"]
@@ -43,5 +41,3 @@ CSV.open("player_crosswalk_#{draft_year}.csv", "w") do |csv|
     csv << ["#{pick[:player_first_name]} #{pick[:player_last_name]}", pick[:mfl_player_id]]
   end
 end
-
-
