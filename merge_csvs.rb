2539fbc2dfa611ac028f31e2bdc19a77e4073bcb
Brock Tillotson <brocktillotson@Brocks-MacBook-Air.local>
2021-06-22 19:21:27 -0700

    all ranks correct

--- merge_csvs.rb
+++ merge_csvs.rb
@@ -30,7 +30,35 @@ csv_string2019 = CSV.generate do |csv|
   end
 end
 
+files = Dir["2020_draft/*.csv"]
+file_contents = files.map { |f| CSV.read(f) }
+
+csv_string2020 = CSV.generate do |csv|
+  csv << csv_headers
+  file_contents.each do |file|
+    file.shift                  # remove the headers of each file
+    file.each do |row|
+      csv << row
+    end
+  end
+end
+
+files = Dir["2021_draft/*.csv"]
+file_contents = files.map { |f| CSV.read(f) }
+
+csv_string2021 = CSV.generate do |csv|
+  csv << csv_headers
+  file_contents.each do |file|
+    file.shift                  # remove the headers of each file
+    file.each do |row|
+      csv << row
+    end
+  end
+end
+
 File.open("all_rankings.csv", "w") do |f|
   f << csv_string2018
   f << csv_string2019
+  f << csv_string2020
+  f << csv_string2021
 end
