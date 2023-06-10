ece2d4337c13940d2b5f81140dd0300195a220da
Arithmetics <brock.m.tillotson@gmail.com>
2023-06-10 13:44:49 -0700

    some 2023 work, cleaning think some stuff disaperaed?

--- get_mfl_draft_data.rb
+++ get_mfl_draft_data.rb
@@ -2,10 +2,11 @@ require "nokogiri"
 require "open-uri"
 require "csv"
 
-# takws a row of mfl draft and converts it to a map/object of a 'draft pick'
+# takes a row of mfl draft and converts it to a map/object of a 'draft pick'
 def convert_row_to_draft_pick(row, league_id, year)
   draft_pick = {}
   cells = row.css("td")
+  puts cells
   draft_pick[:pick_number] = cells[1].text
   draft_pick[:owner_id] = cells[2].css("a")[0]["class"].gsub("franchise_", "").to_i
   draft_pick[:mfl_player_id] = cells[3].css("a")[0]["href"].gsub("javascript:launch_player_modal('#{league_id}','", "").gsub("');", "").to_i
@@ -24,10 +25,12 @@ end
 ##### MAIN #####
 
 league_id = 20896
-draft_year = 2020
+draft_year = 2023
 doc = Nokogiri::HTML(open("http://www67.myfantasyleague.com/#{draft_year}/options?L=#{league_id}&O=17"))
 rows = doc.css(".report")[0].css("tr")
 draft_picks = []
+
+puts rows.length
 
 rows.each_with_index do |row, index|
   if index != 0
